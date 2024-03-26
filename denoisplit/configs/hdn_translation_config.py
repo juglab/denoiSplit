3acbeed1a5e0cea185c7e1b36d28963d55d3394f
Ashesh <ashesh276@gmail.com>
2024-03-26 22:08:13 +0100

    config update

--- denoisplit/configs/hdn_translation_config.py
+++ denoisplit/configs/hdn_translation_config.py
@@ -59,7 +59,7 @@ def get_config():
     loss.kl_loss_formulation = ''
     # loss.mixed_rec_weight = 1
 
-    loss.kl_weight = 0.2
+    loss.kl_weight = 2.0
     loss.kl_annealing = False
     loss.kl_annealtime = 10
     loss.kl_start = -1
