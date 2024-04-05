71bca7156e6bf4752e8ee89c36db2a70e7f3afe1
federico-carrara <federico1.carrara@mail.polimi.it>
2024-04-05 15:32:20 +0200

    Addedd separate file to install dependencies.

---
+++ install_deps.sh
@@ -0,0 +1,20 @@
+mamba install pytorch==1.13.1 torchvision==0.14.1 pytorch-cuda=11.6 -c pytorch -c nvidia -y
+mamba install -c conda-forge pytorch-lightning -y
+mamba install -c conda-forge wandb -y
+mamba install -c conda-forge tensorboard -y
+python -m pip install ml-collections 
+mamba install -c anaconda scikit-learn -y
+mamba install -c conda-forge matplotlib -y
+mamba install -c anaconda ipython -y
+mamba install -c conda-forge tifffile -y
+python -m pip install albumentations
+mamba install -c conda-forge nd2reader -y
+mamba install -c conda-forge yapf -y
+mamba install -c conda-forge isort -y
+python -m pip install pre-commit
+mamba install -c conda-forge czifile -y
+mamba install seaborn -c conda-forge -y
+mamba install nbconvert -y
+mamba install -c anaconda ipykernel -y
+mamba install -c conda-forge czifile -y
+mamba install -c conda-forge zarr -y
