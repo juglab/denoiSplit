eca1996cac4a4544ea2976ccc410eec1ff366ac2
Ashesh <ashesh276@gmail.com>
2025-11-21 16:05:55 +0100

    .sh file updated

--- denoisplit/scripts/htlif.sh
+++ denoisplit/scripts/htlif.sh
@@ -9,4 +9,31 @@ python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/deno
 python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2502/D26-M3-S0-L0/3 --mmse_count=10 --MIXING_WEIGHT=0.9 --data_dir=/group/jug/ashesh/data/diffsplit_HT_LIF/500ms/Ch_B-Ch_D-Ch_BD/ --image_size_for_grid_centers=256 --custom_image_size=512
 
 
+# real inputs
 python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2502/D26-M3-S0-L0/3 --mmse_count=10  --data_dir=/group/jug/ashesh/data/diffsplit_HT_LIF/500ms/Ch_B-Ch_D-Ch_BD/ --notebook=/home/ashesh.ashesh/code/denoiSplit/notebooks/EvaluateRealInput.ipynb --custom_image_size=512 --image_size_for_grid_centers=256
+
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2502/D21-M3-S0-L0/9 --mmse_count=10  --data_dir=/group/jug/ashesh/data/diffsplit_HT_T24/ --notebook=/home/ashesh.ashesh/code/denoiSplit/notebooks/EvaluateRealInput.ipynb --custom_image_size=512 --image_size_for_grid_centers=256
+
+
+
+# hagen
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D7-M3-S0-L0/1 --mmse_count=10 --MIXING_WEIGHT=0.1 --data_dir=/group/jug/ashesh/data/ventura_gigascience --image_size_for_grid_centers=256 --custom_image_size=512
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D7-M3-S0-L0/1 --mmse_count=10 --MIXING_WEIGHT=0.2 --data_dir=/group/jug/ashesh/data/ventura_gigascience --image_size_for_grid_centers=256 --custom_image_size=512
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D7-M3-S0-L0/1 --mmse_count=10 --MIXING_WEIGHT=0.3 --data_dir=/group/jug/ashesh/data/ventura_gigascience --image_size_for_grid_centers=256 --custom_image_size=512
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D7-M3-S0-L0/1 --mmse_count=10 --MIXING_WEIGHT=0.4 --data_dir=/group/jug/ashesh/data/ventura_gigascience --image_size_for_grid_centers=256 --custom_image_size=512
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D7-M3-S0-L0/1 --mmse_count=10 --MIXING_WEIGHT=0.5 --data_dir=/group/jug/ashesh/data/ventura_gigascience --image_size_for_grid_centers=256 --custom_image_size=512
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D7-M3-S0-L0/1 --mmse_count=10 --MIXING_WEIGHT=0.6 --data_dir=/group/jug/ashesh/data/ventura_gigascience --image_size_for_grid_centers=256 --custom_image_size=512
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D7-M3-S0-L0/1 --mmse_count=10 --MIXING_WEIGHT=0.7 --data_dir=/group/jug/ashesh/data/ventura_gigascience --image_size_for_grid_centers=256 --custom_image_size=512
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D7-M3-S0-L0/1 --mmse_count=10 --MIXING_WEIGHT=0.8 --data_dir=/group/jug/ashesh/data/ventura_gigascience --image_size_for_grid_centers=256 --custom_image_size=512
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D7-M3-S0-L0/1 --mmse_count=10 --MIXING_WEIGHT=0.9 --data_dir=/group/jug/ashesh/data/ventura_gigascience --image_size_for_grid_centers=256 --custom_image_size=512
+
+# pavia
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D3-M3-S0-L0/1 --mmse_count=10 --MIXING_WEIGHT=0.1 --data_dir=/group/jug/ashesh/data/ventura_gigascience --image_size_for_grid_centers=256 --custom_image_size=512
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D3-M3-S0-L0/1 --mmse_count=10 --MIXING_WEIGHT=0.2 --data_dir=/group/jug/ashesh/data/ventura_gigascience --image_size_for_grid_centers=256 --custom_image_size=512
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D3-M3-S0-L0/1 --mmse_count=10 --MIXING_WEIGHT=0.3 --data_dir=/group/jug/ashesh/data/ventura_gigascience --image_size_for_grid_centers=256 --custom_image_size=512
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D3-M3-S0-L0/1 --mmse_count=10 --MIXING_WEIGHT=0.4 --data_dir=/group/jug/ashesh/data/ventura_gigascience --image_size_for_grid_centers=256 --custom_image_size=512
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D3-M3-S0-L0/1 --mmse_count=10 --MIXING_WEIGHT=0.5 --data_dir=/group/jug/ashesh/data/ventura_gigascience --image_size_for_grid_centers=256 --custom_image_size=512
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D3-M3-S0-L0/1 --mmse_count=10 --MIXING_WEIGHT=0.6 --data_dir=/group/jug/ashesh/data/ventura_gigascience --image_size_for_grid_centers=256 --custom_image_size=512
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D3-M3-S0-L0/1 --mmse_count=10 --MIXING_WEIGHT=0.7 --data_dir=/group/jug/ashesh/data/ventura_gigascience --image_size_for_grid_centers=256 --custom_image_size=512
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D3-M3-S0-L0/1 --mmse_count=10 --MIXING_WEIGHT=0.8 --data_dir=/group/jug/ashesh/data/ventura_gigascience --image_size_for_grid_centers=256 --custom_image_size=512
+python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D3-M3-S0-L0/1 --mmse_count=10 --MIXING_WEIGHT=0.9 --data_dir=/group/jug/ashesh/data/ventura_gigascience --image_size_for_grid_centers=256 --custom_image_size=512
