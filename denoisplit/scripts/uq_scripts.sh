4ddaa86aec82506de4080b18c0f222f1ea6d127a
Ashesh <ashesh276@gmail.com>
2025-11-26 16:21:36 +0100

    coverage plot not saving calibration plots

--- denoisplit/scripts/uq_scripts.sh
+++ denoisplit/scripts/uq_scripts.sh
@@ -1,29 +1,23 @@
 # BioSR
 uv run python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2502/D16-M3-S0-L0/5 --override_kwargs='{"MIXING_WEIGHT":0.5, "k_forward_pass":2, "data_dir":"/group/jug/ashesh/data/BioSR/"}'
-
 uv run python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2502/D16-M3-S0-L0/5 --override_kwargs='{"MIXING_WEIGHT":0.5, "k_forward_pass":1, "data_dir":"/group/jug/ashesh/data/BioSR/"}'
 
 # Hagen
 uv run python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D7-M3-S0-L0/1 --override_kwargs='{"MIXING_WEIGHT":0.5, "k_forward_pass":2, "data_dir":"/group/jug/ashesh/data/ventura_gigascience/"}'
-
 uv run python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D7-M3-S0-L0/1 --override_kwargs='{"MIXING_WEIGHT":0.5, "k_forward_pass":1, "data_dir":"/group/jug/ashesh/data/ventura_gigascience/"}'
 
 # HTLIF (synthetic)
 uv run python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2502/D26-M3-S0-L0/3 --override_kwargs='{"MIXING_WEIGHT":0.5, "k_forward_pass":2, "data_dir":"/group/jug/ashesh/data/scsplit_HT_LIF/500ms/Ch_B-Ch_D-Ch_BD/"}'
-
 uv run python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2502/D26-M3-S0-L0/3 --override_kwargs='{"MIXING_WEIGHT":0.5, "k_forward_pass":1, "data_dir":"/group/jug/ashesh/data/scsplit_HT_LIF/500ms/Ch_B-Ch_D-Ch_BD/"}'
 
 # HTLIF (real)
 uv run python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2502/D26-M3-S0-L0/3 --override_kwargs='{"MIXING_WEIGHT":0.5, "k_forward_pass":2, "data_dir":"/group/jug/ashesh/data/scsplit_HT_LIF/500ms/Ch_B-Ch_D-Ch_BD/", "use_real_input": true}'
-
 uv run python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2502/D26-M3-S0-L0/3 --override_kwargs='{"MIXING_WEIGHT":0.5, "k_forward_pass":1, "data_dir":"/group/jug/ashesh/data/scsplit_HT_LIF/500ms/Ch_B-Ch_D-Ch_BD/", "use_real_input": true}'
 
 # HTT24 (real)
 uv run python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2502/D21-M3-S0-L0/9/ --override_kwargs='{"MIXING_WEIGHT":0.5, "k_forward_pass":2, "data_dir":"/group/jug/ashesh/data/scsplit_HT_T24/", "use_real_input": true}'
-
 uv run python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2502/D21-M3-S0-L0/9/ --override_kwargs='{"MIXING_WEIGHT":0.5, "k_forward_pass":1, "data_dir":"/group/jug/ashesh/data/scsplit_HT_T24/", "use_real_input": true}'
 
 # paviaATN
-uv run python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D3-M3-S0-L0/1 --override_kwargs='{"MIXING_WEIGHT":0.5, "k_forward_pass":2, "data_dir":"/group/jug/ashesh/data/microscopy", "use_real_input": true}'
-
-uv run python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D3-M3-S0-L0/1 --override_kwargs='{"MIXING_WEIGHT":0.5, "k_forward_pass":1, "data_dir":"/group/jug/ashesh/data/microscopy", "use_real_input": true}'
+uv run python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D3-M3-S0-L0/1 --override_kwargs='{"MIXING_WEIGHT":0.5, "k_forward_pass":2, "data_dir":"/group/jug/ashesh/data/microscopy"}'
+uv run python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2503/D3-M3-S0-L0/1 --override_kwargs='{"MIXING_WEIGHT":0.5, "k_forward_pass":1, "data_dir":"/group/jug/ashesh/data/microscopy"}'
--- notebooks/evaluate_notebook.py
+++ notebooks/evaluate_notebook.py
@@ -3,47 +3,61 @@ import papermill as pm
 from datetime import datetime
 import os
 import json
+from denoisplit.core.data_split_type import DataSplitType
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Run a notebook')
-    parser.add_argument('--notebook', type=str, help='Notebook to run', default='notebooks/CoveragePlot.ipynb')
-    parser.add_argument('--outputdir', type=str, help='Output notebook directory', default='/group/jug/ashesh/UQResults/notebook_results/')
-    parser.add_argument('--ckpt_dir', type=str, help='Checkpoint to use. eg. /group/jug/ashesh/training/disentangle/2406/D25-M3-S0-L8/4')
-    parser.add_argument('--tag_time_flag', type=bool, help='Tag time flag', default=False)
-    parser.add_argument('--override_kwargs', type=json.loads, default='{}', )
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run a notebook")
+    parser.add_argument("--notebook", type=str, help="Notebook to run", default="notebooks/CoveragePlot.ipynb")
+    parser.add_argument(
+        "--outputdir",
+        type=str,
+        help="Output notebook directory",
+        default="/group/jug/ashesh/UQResults/notebook_results/",
+    )
+    parser.add_argument(
+        "--ckpt_dir", type=str, help="Checkpoint to use. eg. /group/jug/ashesh/training/disentangle/2406/D25-M3-S0-L8/4"
+    )
+    parser.add_argument("--data_split_type", type=str, help="Data split type: Train/Val/Test", default="Test")
+    parser.add_argument("--tag_time_flag", type=bool, help="Tag time flag", default=False)
+    parser.add_argument(
+        "--override_kwargs",
+        type=json.loads,
+        default="{}",
+    )
     args = parser.parse_args()
+    assert args.data_split_type in ["Train", "Val", "Test"]
+
     param_dict = args.override_kwargs
     keys = sorted(param_dict.keys())
-    param_str = '_'.join([f'{k}-{param_dict[k]}' for k in keys if k != 'data_dir'])
+    param_str = "_".join([f"{k}-{param_dict[k]}" for k in keys if k != "data_dir"])
+    param_str += f"_{args.data_split_type}"
     ckpt_dir = args.ckpt_dir
-    model_token = '-'.join(ckpt_dir.strip('/').split('/')[-3:])
+    model_token = "-".join(ckpt_dir.strip("/").split("/")[-3:])
     outputdir = os.path.join(args.outputdir, model_token)
     fname = os.path.basename(args.notebook)
-    fname = fname.replace('.ipynb','')
+    fname = fname.replace(".ipynb", "")
     if args.tag_time_flag:
         now = datetime.now().strftime("%Y%m%d.%H.%M")
         fname = f"{fname}_{param_str}_{now}.ipynb"
     else:
         fname = f"{fname}_{param_str}.ipynb"
 
-    param_dict['ckpt_dir'] = args.ckpt_dir
+    param_dict["ckpt_dir"] = args.ckpt_dir
     output_fpath = os.path.join(outputdir, fname)
-    output_config_fpath = os.path.join(outputdir,'config', fname.replace('.ipynb','.txt'))
+    output_config_fpath = os.path.join(outputdir, "config", fname.replace(".ipynb", ".txt"))
     os.makedirs(os.path.dirname(output_config_fpath), exist_ok=True)
     # save the configuration
     # convert args to dict
     args_dict = vars(args)
     # save as json
-    with open(output_config_fpath, 'w') as f:
+    with open(output_config_fpath, "w") as f:
         f.write(str(args_dict))
 
-    print(output_fpath, '\n', output_config_fpath)
-    pm.execute_notebook(
-        args.notebook,
-        output_fpath,
-        parameters = param_dict
-    )
+    param_dict["eval_datasplit_type"] = DataSplitType.from_name(args.data_split_type)
+    param_dict["notebook_output_dir"] = os.path.dirname(output_config_fpath)
+    print(output_fpath, "\n", output_config_fpath, "\n Data Split Evaluated:", args.data_split_type)
+    pm.execute_notebook(args.notebook, output_fpath, parameters=param_dict)
     # python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/denoisplit/2502/D16-M3-S0-L0/5 --data_dir=/group/jug/ashesh/data/BioSR/ --mmse_count=10 --MIXING_WEIGHT=0.1
     # python notebooks/evaluate_notebook.py --ckpt_dir=/group/jug/ashesh/training/disentangle/2507/D21-M3-S0-L0/12 --mmse_count=10 --data_dir=/group/jug/ashesh/data/diffsplit_HT_T24/ --notebook=/home/ashesh.ashesh/code/denoiSplit/notebooks/EvaluateRealInput.ipynb --custom_image_size=512 --image_size_for_grid_centers=256
     # parser = argparse.ArgumentParser(description='Run a notebook')
@@ -62,10 +76,10 @@ if __name__ == '__main__':
     # # get a year-month-day hour-minute formatted string
     # param_str = f"T-{args.MIXING_WEIGHT}_MMSE-{args.mmse_count}"
     # now = datetime.now().strftime("%Y%m%d.%H.%M")
-    # ckpt_dir = args.ckpt_dir    
+    # ckpt_dir = args.ckpt_dir
     # if ckpt_dir[-1] == '/':
     #     ckpt_dir = ckpt_dir[:-1]
-    
+
     # outputdir = os.path.join(args.outputdir, '_'.join(ckpt_dir.split('/')[-3:]))
     # fname = os.path.basename(args.notebook)
     # fname = fname.replace('.ipynb','')
